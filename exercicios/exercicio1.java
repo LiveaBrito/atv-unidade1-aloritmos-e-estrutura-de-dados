40729a102df74952aa7555083b87a51ea126542a
Livea Brito <liveabrito49@yahoo.com>
2026-02-15 13:11:41 -0300

    testando

--- exercicios/exercicio1.java
+++ exercicios/exercicio1.java
@@ -4,5 +4,6 @@ public class exercicio1 {
 
     public static void main(String[] args) throws Exception {
         System.out.println("teste");
+        System.out.println("Apenas testando");
     }
 }
